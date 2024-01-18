c1569ab3b397526cb194a6e18de4cab589bc5716
Marcelo Távora <marcelovtneves@gmail.com>
2024-01-18 18:18:07 -0300

    Feito tratamentos de erros genéricos no back

--- src/main/java/dev/adovgapp/advogapp/controllers/AuthenticationController.java
+++ src/main/java/dev/adovgapp/advogapp/controllers/AuthenticationController.java
@@ -1,5 +1,6 @@
 package dev.adovgapp.advogapp.controllers;
 
+import dev.adovgapp.advogapp.domain.exceptions.ApiRequestException;
 import dev.adovgapp.advogapp.domain.user.AuthenticationDTO;
 import dev.adovgapp.advogapp.domain.user.LoginResponseDTO;
 import dev.adovgapp.advogapp.domain.user.RegisterDTO;
@@ -8,10 +9,10 @@ import dev.adovgapp.advogapp.infra.security.TokenService;
 import dev.adovgapp.advogapp.repositories.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,14 +34,15 @@ public class AuthenticationController {
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(),data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
-
         var token = tokenService.generateToken((User) auth.getPrincipal());
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
-        if(this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+        if(this.repository.findByLogin(data.login()) != null) {
+            throw new ApiRequestException("Usuário Já Cadastrado",HttpStatus.BAD_REQUEST);
+        }
         String encrytpedPassword = new BCryptPasswordEncoder().encode(data.password());
         User newUser = new User(data.login(), encrytpedPassword, data.role());
 
