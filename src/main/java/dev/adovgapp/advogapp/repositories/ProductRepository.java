63e50d2c35dc72305231a4a43163181582734067
Marcelo Távora <marcelovtneves@gmail.com>
2024-01-25 20:09:05 -0300

    Feito remoção da products

--- src/main/java/dev/adovgapp/advogapp/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.adovgapp.advogapp.repositories;
-
-import dev.adovgapp.advogapp.domain.product.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
